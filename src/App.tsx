7ce44dc078a5816243d4fba9e75b0977a79a4cb2
Mrz <mrzdev9@gmail.com>
2024-11-30 01:05:38 -0300

    login agregado

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,30 @@
+import { useState } from "react";
 import Panel from "./components/Panel/Panel"
 import MatrizHorario from "./components/Horario/MatrizHorario"
 import SeccionSeleccionada from "./components/SeccionSeleccionada/SeccionSeleccionada"
+import Login from "./components/Login/Login";
+
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
 
   return (
     <>
-      <div className="flex h-screen bg-gray-100 p-5 gap-5">
-        <Panel />
-        <div className="flex-1 p-6 overflow-auto">
-          <h1 className="text-3xl font-bold mb-4">Horario Académico</h1>
-          <SeccionSeleccionada />
-          <MatrizHorario />
+      {isLoggedIn ? (
+        <div className="flex h-screen bg-gray-100 p-5 gap-5">
+          <Panel />
+          <div className="flex-1 p-6 overflow-auto">
+            <h1 className="text-3xl font-bold mb-4">Horario Académico</h1>
+            <SeccionSeleccionada />
+            <MatrizHorario />
+          </div>
         </div>
-      </div>
+      ) : (
+        <Login onLogin={handleLogin} />
+      )}
     </>
   )
 }
